79bad861c5754b6a1f461ae89eccfdfe759626af
Dao Phuong Anh <dtpanh20101997@gmail.com>
2024-07-19 21:46:43 +0700

    super, this, constructor

---
+++ selenium/javaTester/javaOOP/Topic_00_Demo.java
@@ -0,0 +1,11 @@
+package javaTester.javaOOP;
+
+public class Topic_00_Demo {
+
+    public long shortTimeout = 15;
+    protected long longTimeout = 45;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ selenium/javaTester/javaOOP/Topic_05_This_Super.java
@@ -0,0 +1,34 @@
+package javaTester.javaOOP;
+
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
+public class Topic_05_This_Super extends Topic_00_Demo {
+
+    private WebDriver driver;
+    private int firstNumber;
+    private int secondNumber;
+    private long shortTimeout = 15;
+    private long longTimeout = 45;
+
+    public Topic_05_This_Super(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public void sumNumber() {
+        System.out.println(firstNumber+secondNumber);
+    }
+
+    public void setImplicitWait(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(super.longTimeout));
+    }
+
+    public static void main(String[] args) {
+        Topic_05_This_Super topic = new Topic_05_This_Super(15, 7);
+
+
+
+    }
+}
